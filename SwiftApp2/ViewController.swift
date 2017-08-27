5cc777dd1a55b1b6a71dbb6217f0ac414e48022a
Colin Beltz <colinbeltz@gmail.com>
2017-08-27 18:23:23 -0500

    Commit

---
+++ SwiftApp2/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SwiftApp2
+//
+//  Created by Colin on 8/27/17.
+//  Copyright © 2017 Colin. All rights reserved..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+       
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
+    @IBAction func secondButtonTapped(_ sender: Any) {
+        theLabel.text = "Buttons are cool!"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

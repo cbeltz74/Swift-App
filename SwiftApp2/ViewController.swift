a21af2a227668f12abb5627dce09154d8fe66a56
Colin Beltz <colinbeltz@gmail.com>
2017-08-27 18:58:57 -0500

    third commit

--- SwiftApp2/ViewController.swift
+++ SwiftApp2/ViewController.swift
@@ -20,8 +20,8 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        print(text1.text!)
-        print(text2.text!)
+       theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        
         
     }
     
